7ec7e0f6df01da65defdfcf5de65cef3e4a5a18a
Grigor Hovsepyan <theo.grigor787@gmail.com>
2025-09-11 15:12:31 +0400

    Homework1

--- src/homework/homework1/FigurePainter.java
+++ src/homework/homework1/FigurePainter.java
@@ -41,5 +41,27 @@ public class FigurePainter {
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println();
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j <= 5-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 4; j >= i; j--) {
+                System.out.print(" *");
+            }
+
+            System.out.println();
+        }
     }
 }
