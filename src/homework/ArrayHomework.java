d8452b6aef8fce945093a8a1a3f1373524b7429f
Grigor Hovsepyan <theo.grigor787@gmail.com>
2025-09-18 20:06:25 +0400

    ArrayHomework

---
+++ src/homework/ArrayHomework.java
@@ -0,0 +1,52 @@
+package homework;
+
+
+public class ArrayHomework {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        int n = 6;
+        int equals = 0;
+        int sumD = 0;
+        String text = "բարևաշխարհ";
+        char[] chars = text.toCharArray();
+        String textD = "աէըիօւ"; // ու-ն ուրիշ ձևվի 2 սիմվոլ է
+        char[] charsD = textD.toCharArray();
+        // Task 1
+        for (int i : nums) {
+            if (i == n) { // i == 6 Ելի որ կստացվի, բայց խնդրի մեյջ կար n = 6
+                sum++;
+            }
+        }
+        System.out.println("The quantity of numbers that equals to n are - " + sum);
+
+        //Task 2
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
+        //Task 3
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i != j) {
+                    equals++;
+                    break;
+                }
+            }
+        }
+        System.out.println("The quantity of equals in array is - " + equals);
+
+        //Task 4
+        for (char i : chars) {
+            for (char j : charsD) {
+                if (i == j) {
+                    sumD++;
+                }
+            }
+        }
+        //Զուտ անմիջապես char-Երի մասիվի մեջ ներմուծելով սիմվոլները կխաոներ, էդ պատճաոով String-ը ToCharArray Դարձրի, նոր աշխատավ
+        System.out.println("The quantity of vowels in array equals - " + sumD);
+    }
+}
