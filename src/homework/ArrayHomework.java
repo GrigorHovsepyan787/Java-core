170a72ad04723b39bd0bb4035b1dfe193e6fe95e
Grigor Hovsepyan <theo.grigor787@gmail.com>
2025-09-18 21:05:05 +0400

    ArrayHomework

--- src/homework/ArrayHomework.java
+++ src/homework/ArrayHomework.java
@@ -29,7 +29,7 @@ public class ArrayHomework {
 
         //Task 3
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
+            for (int j = 0; j < nums.length; j++) {
                 if (nums[i] == nums[j] && i != j) {
                     equals++;
                     break;
