118e0a3675fa716b28c6c78751b7fc08620e9353
Grigor Hovsepyan <theo.grigor787@gmail.com>
2025-10-27 20:56:44 +0400

    Employee

--- src/homework/employee/EmployeeDemo.java
+++ src/homework/employee/EmployeeDemo.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class EmployeeDemo implements CommandsEmp {
     private static final Scanner scan = new Scanner(System.in);
     private static final EmployeeStorage employeeStorage = new EmployeeStorage();
-
+    private static boolean isRun;
     public static void main(String[] args) {
-        boolean isRun = true;
+        isRun = true;
         while (isRun){
             CommandsEmp.printCommands();
             String command = scan.nextLine();
             switch (command) {
-                case EXIT -> isRun = false;
+                case EXIT -> exit();
                 case ADD -> addEmployee();
                 case PRINT -> printAllEmployee();
                 case SEARCH_ID -> searchID();
@@ -21,27 +21,34 @@ public class EmployeeDemo implements CommandsEmp {
             }
         }
     }
-
+    private static void exit(){
+        System.out.println("Exiting the program");
+        isRun = false;
+    }
     private static void addEmployee() {
-        System.out.println("Input employee name");
-        String employeeName = scan.nextLine();
-        System.out.println("Input employee surname");
-        String employeeSurname = scan.nextLine();
-        System.out.println("Input employee ID");
-        String employeeID = scan.nextLine();
-        if(!employeeStorage.isIdBusy(employeeID)) {
-            System.out.println("Input employee salary");
-            Double employeeSalary = scan.nextDouble();
-            System.out.println("Input the company of employee");
-            String employeeCom = scan.nextLine();
-            System.out.println("Input employee position");
-            String employeePos = scan.nextLine();
-            Employee emp = new Employee(employeeName, employeeSurname, employeeID, employeeSalary, employeeCom, employeePos);
-            employeeStorage.add(emp);
-            System.out.println("Employee has been successfully added!");
+        try {
+            System.out.println("Input employee name");
+            String employeeName = scan.nextLine();
+            System.out.println("Input employee surname");
+            String employeeSurname = scan.nextLine();
+            System.out.println("Input employee ID");
+            String employeeID = scan.nextLine();
+            if (!employeeStorage.isIdBusy(employeeID)) {
+                System.out.println("Input employee salary");
+                Double employeeSalary = scan.nextDouble();
+                System.out.println("Input the company of employee");
+                String employeeCom = scan.nextLine();
+                System.out.println("Input employee position");
+                String employeePos = scan.nextLine();
+                Employee emp = new Employee(employeeName, employeeSurname, employeeID, employeeSalary, employeeCom, employeePos);
+                employeeStorage.add(emp);
+                System.out.println("Employee has been successfully added!");
+            } else {
+                System.out.println("Id is already busy!");
+            }
         }
-        else{
-            System.out.println("Id is already busy!");
+        catch (java.util.InputMismatchException e){
+            System.err.println("Wrong value for double");
         }
     }
 
